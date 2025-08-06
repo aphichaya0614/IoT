083a4c8e06350d1953a00fe64c04796a204a5255
aphichaya0614 <aphichayakannapooti@gmail.com>
2025-08-06 14:22:12 +0700

    Add files via upload

---
+++ 7-Segment/7-Segment_1.ino
@@ -0,0 +1,123 @@
+int segmentA = A0;
+int segmentB = A1;
+int segmentC = A2;
+int segmentD = 2;
+int segmentE = 4;
+int segmentF = 7;
+int segmentG = 8;
+int segmentDP = 13;
+void setup() {
+  pinMode(segmentA, OUTPUT);
+  pinMode(segmentB, OUTPUT);
+  pinMode(segmentC, OUTPUT);
+  pinMode(segmentD, OUTPUT);
+  pinMode(segmentE, OUTPUT);
+  pinMode(segmentF, OUTPUT);
+  pinMode(segmentG, OUTPUT);
+  pinMode(segmentDP, OUTPUT);
+  pinMode(12, INPUT_PULLUP);
+}
+void loop() {
+  displayNumber();
+}
+void displayNumber() {
+  int sw = digitalRead(12);
+  if (sw == 0) {
+    for (int digit = 9; digit >= 1; digit--) {
+      displaySegment(digit);  // แสดงผลบน 7-Segment ขนาด 1 หลัก
+      delay(500);             // หน่วงเวลา 0.5 วินาที
+    }
+  }
+
+  else {
+    for (int digit = 1; digit <= 9; digit++) {
+      displaySegment(digit);  // แสดงผลบน 7-Segment ขนาด 1 หลัก
+      delay(500);             // หน่วงเวลา 0.5 วินาที
+    }
+  }
+}
+void displaySegment(int numberToDisplay) {
+  switch (numberToDisplay) {
+    case 1:  // แสดงผลเลข 1
+      digitalWrite(segmentA, LOW);
+      digitalWrite(segmentB, HIGH);
+      digitalWrite(segmentC, HIGH);
+      digitalWrite(segmentD, LOW);
+      digitalWrite(segmentE, LOW);
+      digitalWrite(segmentF, LOW);
+      digitalWrite(segmentG, LOW);
+      break;
+    case 2:  // แสดงผลเลข 2
+      digitalWrite(segmentA, HIGH);
+      digitalWrite(segmentB, HIGH);
+      digitalWrite(segmentC, LOW);
+      digitalWrite(segmentD, HIGH);
+      digitalWrite(segmentE, HIGH);
+      digitalWrite(segmentF, LOW);
+      digitalWrite(segmentG, HIGH);
+      break;
+    case 3:  // แสดงผลเลข 3
+      digitalWrite(segmentA, HIGH);
+      digitalWrite(segmentB, HIGH);
+      digitalWrite(segmentC, HIGH);
+      digitalWrite(segmentD, HIGH);
+      digitalWrite(segmentE, LOW);
+      digitalWrite(segmentF, LOW);
+      digitalWrite(segmentG, HIGH);
+      break;
+    case 4:  // แสดงผลเลข 4
+      digitalWrite(segmentA, LOW);
+      digitalWrite(segmentB, HIGH);
+      digitalWrite(segmentC, HIGH);
+      digitalWrite(segmentD, LOW);
+      digitalWrite(segmentE, LOW);
+      digitalWrite(segmentF, HIGH);
+      digitalWrite(segmentG, HIGH);
+      break;
+    case 5:  // แสดงผลเลข 5
+      digitalWrite(segmentA, HIGH);
+      digitalWrite(segmentB, LOW);
+      digitalWrite(segmentC, HIGH);
+      digitalWrite(segmentD, HIGH);
+      digitalWrite(segmentE, LOW);
+      digitalWrite(segmentF, HIGH);
+      digitalWrite(segmentG, HIGH);
+      break;
+    case 6:  // แสดงผลเลข 6
+      digitalWrite(segmentA, HIGH);
+      digitalWrite(segmentB, LOW);
+      digitalWrite(segmentC, HIGH);
+      digitalWrite(segmentD, HIGH);
+      digitalWrite(segmentE, HIGH);
+      digitalWrite(segmentF, HIGH);
+      digitalWrite(segmentG, HIGH);
+      break;
+    case 7:  // แสดงผลเลข 7
+      digitalWrite(segmentA, HIGH);
+      digitalWrite(segmentB, HIGH);
+      digitalWrite(segmentC, HIGH);
+      digitalWrite(segmentD, LOW);
+      digitalWrite(segmentE, LOW);
+      digitalWrite(segmentF, LOW);
+      digitalWrite(segmentG, LOW);
+      break;
+    case 8:  // แสดงผลเลข 8
+      digitalWrite(segmentA, HIGH);
+      digitalWrite(segmentB, HIGH);
+      digitalWrite(segmentC, HIGH);
+      digitalWrite(segmentD, HIGH);
+      digitalWrite(segmentE, HIGH);
+      digitalWrite(segmentF, HIGH);
+      digitalWrite(segmentG, HIGH);
+      break;
+    case 9:  // แสดงผลเลข 9
+      digitalWrite(segmentA, HIGH);
+      digitalWrite(segmentB, HIGH);
+      digitalWrite(segmentC, HIGH);
+      digitalWrite(segmentD, HIGH);
+      digitalWrite(segmentE, LOW);
+      digitalWrite(segmentF, HIGH);
+      digitalWrite(segmentG, HIGH);
+      break;
+  }
+}
