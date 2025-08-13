cdd41e5e555065053e5b82a3a2f3de3638acb419
aphichaya0614 <aphichayakannapooti@gmail.com>
2025-08-13 16:20:56 +0700

    Add files via upload

---
+++ IRremote/IRremote_1.ino
@@ -0,0 +1,34 @@
+#include <IRremote.h>  // Include the library
+#define RECV 11        // IR receiver pin 11
+IRrecv irrecv(RECV);
+decode_results results;
+void setup() {
+  Serial.begin(9600);    // Message will be sent to PC
+  irrecv.enableIRIn();   // Start the receiver
+  irrecv.blink13(true);  // pin 13 blink
+}
+void loop() {
+  if (irrecv.decode(&results))  // Received IR signal
+  {
+    switch (results.decode_type) {
+      case NEC: Serial.print("NEC: "); break;
+      case SONY: Serial.print("SONY: "); break;
+      case SHARP: Serial.print("SHARP: "); break;
+      case PANASONIC: Serial.print("PANASONIC: "); break;
+      case JVC: Serial.print("JVC: "); break;
+      case SANYO: Serial.print("SANYO: "); break;
+      case MITSUBISHI: Serial.print("MITSUBISHI: "); break;
+      case RC5: Serial.print("Philips RC5: "); break;
+      case RC6: Serial.print("Philips RC6: "); break;
+      case DISH: Serial.print("DISH: "); break;
+      case DENON: Serial.print("DENON: "); break;
+      case SAMSUNG: Serial.print("SAMSUNG: "); break;
+      case LG: Serial.print("LG: "); break;
+      case UNKNOWN: Serial.print("UNKNOWN: "); break;
+    }
+    Serial.println(results.value, HEX);  // Print raw data
+    irrecv.resume();                     // Enable receiving of the next value
+  }
+}
+
+// 1. ให้ต่อวงจรใช้ Infrared Receiver Modules for PCM Remote Control Systems TSOP1838 เข้าที่ขา D11 และป้อนโปรแกรมดังตัวอย่าง โดยใช้ไลบรารี IRremote.h แล้วทดลองการทำงานโดยใช้ Remote แบบต่างๆ และให้อธิบายผลลัพธ์ที่ได้ //
